156ee06a69e5e39286c77660db5183628b360d36
Wencel T Kust <iamtaillow@gmail.com>
2018-02-24 13:42:04 -0600

    Added time and timespan.

---
+++ StaffAssignmentProgram.Common/Time.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAP.Common
+{
+	public class Time
+	{
+		public DayOfWeek Day { get; set; }
+
+		private double _hour;
+		public double Hour
+		{
+			get => _hour;
+			set
+			{
+				if (value >= 0 && value < 24)
+					_hour = value;
+				else
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		public string GetHourAsClockTime(bool getAsMilitary = false)
+		{
+			int hour = 0;
+			int minute = 0;
+			string postfix = String.Empty;
+
+			hour = (int)Hour;
+			minute = (int)(Hour - hour) * 60;
+
+			if (!getAsMilitary)
+			{
+				postfix = (hour >= 12) ? "pm" : "am";
+				hour %= 12;
+				if (hour == 0)
+					hour += 12;
+			}
+
+			return $"{hour}:{minute} {postfix}";
+
+		}
+
+		public bool Equals(Time other)
+		{
+			return other.Day == this.Day && other.Hour == this.Hour;
+		}
+
+		public static bool operator > (Time first, Time second)
+		{
+			if ((int)first.Day > (int)second.Day)
+				return true;
+			if ((int)first.Day < (int)second.Day)
+				return false;
+			return first.Hour > second.Hour;
+		}
+		public static bool operator < (Time first, Time second)
+		{
+			return !(first > second) && !first.Equals(second); 
+		}
+	}
+}
---
+++ StaffAssignmentProgram.Common/TimeSpan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAP.Common
+{
+	public class TimeSpan
+	{
+		public TimeSpan(Time startTime, Time endTime)
+		{
+			if (endTime > startTime)
+				throw new ArgumentOutOfRangeException();
+
+			EndTime = endTime;
+			StartTime = startTime;
+		}
+		public Time StartTime { get; private set; }
+		public Time EndTime { get; private set; }
+	}
+}
